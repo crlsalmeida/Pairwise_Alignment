933ed4ce382682cd2d8123324f1e79e074b4698d
crlsalmeida <61167682+crlsalmeida@users.noreply.github.com>
2020-02-24 14:00:20 -0500

    Update Smith_Waterman.py

--- Smith_Waterman.py
+++ Smith_Waterman.py
@@ -35,8 +35,6 @@ def SmithWaterman(s1,s2, GapScore = GapScore, ScrMtrx = NucScrTbl, SeqType = Nuc
         Define ScrMtrx as either 'NucScrTbl' for nucleotide sequences
         Or 'PAM250', 'BLOSUM62', etc. for AA sequences provided the tables were loaded as described in the 'AASeq' function
     A GapScore is required to use this function, whether you are aligning amino acids or nucleotides
-        If you are aligning AAs, simply assign a dummy value
-        As a safety precaution, this function will check if you are aligning AA or nucleotides, and adjust the GapScore accordingly
     
     '''
     # Set up values for Matrices
@@ -45,10 +43,7 @@ def SmithWaterman(s1,s2, GapScore = GapScore, ScrMtrx = NucScrTbl, SeqType = Nuc
     m = len(s2) # m is associated with y, j
     s1L = list(s1.strip()) # convert s1 string to list
     s2L = list(s2.strip()) # convert s2 string to list
-    if SeqType is NucSeq:
-        d = GapScore
-    else:
-        d = ScrMtrx[0,-1]
+    d = GapScore
         
     # Create Score Matrix
     
